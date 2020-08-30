be3e19900b6c3ca35dd05d6e2ad92cd9639f3d43
Жасулан Садыков <sadykovzhassulan@gmail.com>
2020-08-30 23:23:20 +0600

    Первый залив проекта

---
+++ Models/PageData.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AidaCarParts.Models
+{
+    public class PageData
+    {
+        public PageViewModel GetPageData(int page = 1, int sectionAndSubsectionId = 7)
+        {
+            var context = new Context();
+            int pageSize = 17; // количество объектов на страницу
+            IEnumerable<Part> phonesPerPages = context.Parts.ToList().Where(p=>p.SectionAndSubsectionId == sectionAndSubsectionId).Skip((page - 1) * pageSize).Take(pageSize);
+            PageInfo pageInfo = new PageInfo { PageNumber = page, PageSize = pageSize, TotalItems = context.Parts.ToList().Where(p => p.SectionAndSubsectionId == sectionAndSubsectionId).Count() };
+            PageViewModel ivm = new PageViewModel { PageInfo = pageInfo, Parts = phonesPerPages };
+            return ivm;
+        }
+    }
+    public class PageInfo
+    {
+        public int PageNumber { get; set; } // номер текущей страницы
+        public int PageSize { get; set; } // кол-во объектов на странице
+        public int TotalItems { get; set; } // всего объектов
+        public int TotalPages  // всего страниц
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / PageSize); }
+        }
+    }
+    public class PageViewModel
+    {
+        public IEnumerable<Part> Parts { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
